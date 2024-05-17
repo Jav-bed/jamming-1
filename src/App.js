65b7bd918e7a66ca9e27acbfd44e1bb7352fa62a
Javier Bedoya <javier.bedoya1@gmail.com>
2024-05-16 23:08:48 -0600

    fixed file tree and module imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import SearchBar from "./components/SearchBar/Searchbar/SearchBar";
+import SearchResults from "./components/SearchResults/SearchResults";
 
 function App() {
     return (
         <div className="App">
             <SearchBar />
+            <SearchResults />
         </div>
     );
 }
